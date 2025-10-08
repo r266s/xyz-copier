76626649975d732e1bbdfaf6dd896eda3edb943e
r266 <134190325+r266s@users.noreply.github.com>
2025-10-08 14:20:15 +0800

    Small changes, improved command feedback

--- src/main/java/net/r266/xyz/Commands/CopyCoordinate.java
+++ src/main/java/net/r266/xyz/Commands/CopyCoordinate.java
@@ -7,9 +7,13 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.entity.Entity;
-import net.minecraft.text.Text;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Style;
+import net.minecraft.util.Formatting;
+import static net.minecraft.text.Text.literal;
 
 public class CopyCoordinate {
   public static String Arg1 = "Axis";
@@ -17,7 +21,7 @@ public class CopyCoordinate {
 
   public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess) {
     dispatcher.register(ClientCommandManager.literal("CopyCoordinate")
-            .then(ClientCommandManager.argument(Arg1, StringArgumentType.string())
+            .then(ClientCommandManager.argument(Arg1, StringArgumentType.string()).executes(CopyCoordinate::Run)
                     .then(ClientCommandManager.argument(Arg2, BoolArgumentType.bool()).executes(CopyCoordinate::Run))));
   }
 
@@ -33,13 +37,19 @@ public class CopyCoordinate {
   private static int Run(CommandContext<FabricClientCommandSource> ClientCommandSourceCommandContext) throws CommandSyntaxException {
     String AllowedAxis = "XYZ".toUpperCase();
     String SelectedAxis = StringArgumentType.getString(ClientCommandSourceCommandContext, Arg1).toUpperCase();
-    boolean AddAxisHeaders = BoolArgumentType.getBool(ClientCommandSourceCommandContext, Arg2);
+    boolean AddAxisHeaders;
+
+    try {
+      AddAxisHeaders = BoolArgumentType.getBool(ClientCommandSourceCommandContext, Arg2);
+    } catch (IllegalArgumentException e) {
+      AddAxisHeaders = false;
+    }
 
     FabricClientCommandSource ClientCommandSource = ClientCommandSourceCommandContext.getSource();
     Entity Player = ClientCommandSource.getEntity();
 
     if (Player == null) {
-      ClientCommandSource.sendFeedback(Text.literal("Player not exist"));
+      ClientCommandSource.sendFeedback(literal("Player not exist"));
       return -1;
     }
 
@@ -60,20 +70,31 @@ public class CopyCoordinate {
 
           Coords += (AddAxisHeaders) ? "%s: %.2f ".formatted(SelectedAxi, CorrectAxis) : "%.2f ".formatted(CorrectAxis);
         } else {
-          ClientCommandSource.sendFeedback(Text.literal("'%s' is not axis!".formatted(SelectedAxi)));
+          ClientCommandSource.sendFeedback(literal("'%s' is not axis!".formatted(SelectedAxi)));
           return -1;
         }
       }
 
+      MinecraftClient WindowClient = MinecraftClient.getInstance();
+
       String finalCoords = Coords;
-      ClientCommandSource.sendFeedback(Text.literal(finalCoords));
+
+      if (WindowClient != null) {
+        WindowClient.keyboard.setClipboard(finalCoords);
+        ClientCommandSource.sendFeedback(literal("Successfully copied to clipboard!")
+                .formatted(Formatting.GREEN, Formatting.BOLD, Formatting.UNDERLINE));
+      } else {
+        ClientCommandSource.sendFeedback(
+                literal("Cannot Access clipboard, Click me to copy the " + SelectedAxis + ".")
+                        .setStyle(Style.EMPTY.withClickEvent(new ClickEvent.CopyToClipboard(finalCoords))));
+      }
 
       return 1;
     } else {
       if (SelectedAxis.length() > 3) {
-        ClientCommandSource.sendFeedback(Text.literal("Trying to access the 4th dimension?"));
+        ClientCommandSource.sendFeedback(literal("Trying to access the 4th dimension?"));
       } else {
-        ClientCommandSource.sendFeedback(Text.literal("Select a Axis!"));
+        ClientCommandSource.sendFeedback(literal("Select a Axis!"));
       }
       return -1;
     }
